fef4ee594aac5596fda3d3c1678794b0ea604a04
suryamani lodhi <57284410+suryamanilodhi@users.noreply.github.com>
2022-11-24 18:29:36 +0530

    IIFE  FUNCTION IN JAVASCRIPT

---
+++ IIFE funcrtion.js
@@ -0,0 +1,7 @@
+//IIFE FUNCTION:(immediate  invoked  function expression)
+//function that call itself is called  iifi function 
+//pure function+iife  function+anonymous  function
+ let ans=(function(num1,num2){
+    return num1+num2;
+})(3,4);
+console.log(ans);
